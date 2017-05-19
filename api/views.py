d460a0da3311b2ca724249ab87360ed83596ed1b
agiledots <agiledots@gmail.com>
2017-05-19 10:41:30 +0900

    api

--- api/views.py
+++ api/views.py
@@ -8,20 +8,21 @@ from template.models import Templates, Images
 from .serializer import ImageSerializer, TemplateSerializer, TransactionSerializer
 from .models import Transaction;
 
-
 class TemplateViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+
     queryset = Templates.objects.all();
     serializer_class = TemplateSerializer;
 
 class ImageViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+
     queryset = Images.objects.all();
     serializer_class = ImageSerializer;
 
 class TransactionViewSet(viewsets.ModelViewSet):
     authentication_classes = []
 
-    queryset = Transaction.objects.all();
+    queryset = Transaction.objects.all().order_by('id').reverse();
     serializer_class = TransactionSerializer;
-
-
 
