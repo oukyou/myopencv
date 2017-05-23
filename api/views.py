3eaf70ec95fa80be41416f50d23d5a3a7a9ca475
agiledots <agiledots@gmail.com>
2017-05-23 15:25:53 +0900

    テンプレートIDで画像を検索する

--- api/views.py
+++ api/views.py
@@ -8,6 +8,8 @@ from template.models import Templates, Images
 from .serializer import ImageSerializer, TemplateSerializer, TransactionSerializer
 from .models import Transaction;
 
+from rest_framework import filters, generics
+
 class TemplateViewSet(viewsets.ModelViewSet):
     authentication_classes = []
 
@@ -20,9 +22,34 @@ class ImageViewSet(viewsets.ModelViewSet):
     queryset = Images.objects.all();
     serializer_class = ImageSerializer;
 
+    def get_queryset(self):
+        queryset = Images.objects.all().order_by('rank')
+        template = self.request.query_params.get('template', None)
+        if template is not None:
+            queryset = queryset.filter(template=template)
+
+        return queryset
+
+
 class TransactionViewSet(viewsets.ModelViewSet):
     authentication_classes = []
 
     queryset = Transaction.objects.all().order_by('id').reverse();
     serializer_class = TransactionSerializer;
 
+
+# class TemplateImageList(generics.ListAPIView):
+#     model = Images
+#     serializer_class = ImageSerializer
+#
+#     # Show all of the PASSENGERS in particular WORKSPACE
+#     # or all of the PASSENGERS in particular AIRLINE
+#     def get_queryset(self):
+#         queryset = Images.objects.all()
+#
+#         template_id = self.request.query_params.get('template_id')
+#
+#         if template_id:
+#             queryset = queryset.filter(template=template_id)
+#
+#         return queryset
